6ca031489d1dc45530185eeb0b5c3f6f27b3e702
Quatton <atomicatives@gmail.com>
2023-06-05 02:39:04 +0900

    fix(type/tabs.ts): silence Enumerate/IntRange ts error
    Type instantiation is excessively deep and possibly infinite. ts(2589)

--- src/type/tabs.ts
+++ src/type/tabs.ts
@@ -1,16 +1,18 @@
-export type Enumerate<
-  N extends number,
-  Acc extends number[] = []
-> = Acc['length'] extends N
-  ? Acc[number]
-  : Enumerate<N, [...Acc, Acc['length']]>
+// export type Enumerate<
+//   N extends number,
+//   Acc extends number[] = []
+// > = Acc['length'] extends N
+//   ? Acc[number]
+//   : Enumerate<N, [...Acc, Acc['length']]>
 
-export type IntRange<F extends number, T extends number> = Exclude<
-  Enumerate<T>,
-  Enumerate<F>
->
+// export type IntRange<F extends number, T extends number> = Exclude<
+//   Enumerate<T>,
+//   Enumerate<F>
+// >
 
-export type fretRange = IntRange<-1, 20>
+// export type fretRange = IntRange<-1, 20>
+
+export type fretRange = -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20
 
 export type EachBarProps = {
   noteBarList: Array<Array<number>>
